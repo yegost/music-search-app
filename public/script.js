f62a43ef45cd432140838aeb385ee51ad4e10cf2
yegost <caza.alexandru@yahoo.com>
2026-03-17 05:28:43 +0000

    add artist profile and styling

--- public/script.js
+++ public/script.js
@@ -1,25 +1,31 @@
 const searchBar = document.getElementById('search-input');
 const results = document.getElementById('results');
+const headerText = document.getElementById('header-text');
+
+let debounceTimer;
 
 searchBar.addEventListener('input', async () => {
+    clearTimeout(debounceTimer)
     const artist = searchBar.value;
     if (!artist) {
         results.innerHTML = '';
         return;
     };
 
-    const res = await fetch(`/search?artist=${artist}`);
-    const data = await res.json()
-
-    results.innerHTML = ''
-    data.forEach(artist => {
-        results.innerHTML += `
-            <div class="artist-result" data-id="${artist.id}">
-                <img src="${artist.images[2]?.url || '/images/placeholder.png'}" alt="${artist.name}" />
-                <p>${artist.name}</p>
-            </div>
-        `
-    });
+    debounceTimer = setTimeout(async () => {
+        const res = await fetch(`/search?artist=${artist}`);
+        const data = await res.json()
+        
+        results.innerHTML = ''
+        data.forEach(artist => {
+            results.innerHTML += `
+                <div class="artist-result" data-id="${artist.id}">
+                    <img src="${artist.images[2]?.url || '/images/placeholder.png'}" alt="${artist.name}" />
+                    <p>${artist.name}</p>
+                </div>
+            `
+        });
+    }, 500)
 })
 
 searchBar.addEventListener('blur', () => {
@@ -40,30 +46,35 @@ results.addEventListener('click', async (e) => {
     const albumsData = await albumsRes.json()
 
     results.innerHTML = ''
-/*
-    results.innerHTML = `
-        <div id="artist-page">
-            <div id="artist-header">
-                <img id="artist-img" src="${image}" />
-                <div>
-                    <h2>${name}</h2>
-                </div>
-            </div>
-            <div id="albums-section">
-                <h3>Albums</h3>
-                <div id="albums-grid">
-                    ${albumsData.items.map((album) => `
-                        <div class="album-card" data-id="${album.id}">
-                            <img src="${album.images[0]?.url}" alt="${album.name}" />
-                            <h5>${album.name.length > 32 
-                                ? album.name.slice(0, 31) + '...' 
-                                : album.name}</h5>
-                            <p>${album.release_date.slice(0, 4)}</p>
-                        </div>    
-                    `).join('')}
-                </div>
-            </div>
+    searchBar.value = ''
+
+    const artistPage = document.getElementById('artist-page')
+    const artistHeader = document.getElementById('artist-header')
+    const albumsSection = document.getElementById('albums-section')
+
+    artistHeader.innerHTML = `
+        <img id="artist-img" src="${image}" alt="${name}" />
+        <div>
+            <h2>${name}</h2>
         </div>
     `
-*/
+    
+    albumsSection.innerHTML = `
+        <h3>Albums</h3>
+        <div id="albums-grid">
+            ${albumsData.items.map(album => `
+                    <div class="album-card" data-id="${album.id}">
+                        <img src="${album.images[0]?.url || '/public/images/placeholder.png'}" alt="${album.name}" />
+                        <h4>${album.name.length > 31
+                            ? album.name.slice(0, 31) + '...'
+                            : album.name
+                        }</h4>
+                        <p>${album.release_date.slice(0, 4)}</p>
+                    </div>
+                `).join('')}
+        </div>
+    `
+
+    headerText.classList.add('hidden')
+    artistPage.classList.remove('hidden')
 })
